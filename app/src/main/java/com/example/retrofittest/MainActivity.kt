001899bac0577409c9078ebe04544f90c5d88811
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-27 23:56:44 -0300

    Add response from retrofit

--- app/src/main/java/com/example/retrofittest/MainActivity.kt
+++ app/src/main/java/com/example/retrofittest/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.retrofittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val remote = RetrofitClient.createService(PostService::class.java)
+        val call: Call<List<PostModel>> = remote.list()
+
+        val response = call.enqueue(object : Callback<List<PostModel>>{
+            override fun onFailure(call: Call<List<PostModel>>, t: Throwable) {
+                val s = t.message
+            }
+
+            override fun onResponse(call: Call<List<PostModel>>, response: Response<List<PostModel>>) {
+                val s = response.body()
+            }
+
+        })
     }
 }
